b7825d93f14ac7b989d4835b68339b5ae9ffd629
MartinRodriguez1992 <132406183+MartinRodriguez1992@users.noreply.github.com>
2023-06-17 13:39:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var desplegarMenu = document.getElementById("desplegar");
+var contraerMenu = document.getElementById("contraer");
+
+
+desplegarMenu.addEventListener('click',()=>{
+    document.getElementById("desplegable").style.top="0em";
+    document.getElementById("desplegable").style.opacity="1";
+    document.getElementById("desplegar").style.display="none";
+    document.getElementById("contraer").style.display="block";
+});
+
+contraerMenu.addEventListener('click',()=>{
+    document.getElementById("desplegable").style.top="-8em";
+    document.getElementById("desplegable").style.opacity="0";
+    document.getElementById("desplegar").style.display="block";
+    document.getElementById("contraer").style.display="none";
+});
